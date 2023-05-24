209ca71c9c4e5c721b155f5f391a39c20230eda5
Jigyashu Kumar Gupta <75210289+jigyashu765@users.noreply.github.com>
2023-05-24 13:55:07 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('heading').innerHTML="Light On / Off Project";
+
+function onLight() {
+    document.getElementById('myimg').src='Img/pic_bulbon.gif';
+}
+
+function offLight() {
+    document.getElementById('myimg').src='Img/pic_bulboff.gif';
+}
